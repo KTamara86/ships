3ce3f4a726097cc5273f024e6949627d1fba71ab
Kukta Tamara <kukta.tamara@gmail.com>
2023-03-28 20:25:17 +0200

    Fejrész hozzáadva

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+/*
+File: app.js
+Author: Kukta Tamara
+Copyright: 2023, Kukta Tamara
+Group: SZOFT I-1-E
+Date: 2023-03-28
+Github: https://github.com/KTamara86/
+Licenc: GNU GPL
+*/
+
 const doc = {
     tbody: null
 };
